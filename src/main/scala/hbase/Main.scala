c730ba17984bc572002d92850186582ca33f4358
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-02-07 10:04:56 +0100

    Main is now general

--- src/main/scala/hbase/Main.scala
+++ src/main/scala/hbase/Main.scala
@@ -7,13 +7,32 @@ object Main {
   implicit val config = HBaseConfiguration.create()
 
   def main(args: Array[String]) {
-    val admin = new HBaseAdmin(config)
-    val tables = admin.listTables()
-    tables.foreach(println)
-  
-    val table = Table("shorturl")
-    val result = table.get[String]("http://atmlb.com/7NG4sm")
-    println(result)
+    args match {
+      case Array("list") => {
+        borrow(new HBaseAdmin(config)) { admin =>
+          val tables = admin.listTables()
+          tables.foreach(t => println(t.getNameAsString))
+        }
+      }
+      case Array("get", tableName, key) => {
+        borrow(Table(tableName)) { table =>
+          val result = table.get[String](key)
+          println(result.flatMap(_.getValue[String]).getOrElse("Value for %s not found".format(key)))
+        }
+      }
+      case _ => {
+        println("Command missing or unknown")
+        sys.exit(1)
+      }
+    }
+  }
+
+  private def borrow[A <: java.io.Closeable](obj: A)(f: A => Unit) {
+    try {
+      f(obj)
+    } finally {
+      obj.close()
+    }
   }
   
 }
