1f6ac98a5cc9b8fcaca1aed929035b7be1940171
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-02-10 15:57:51 +0100

    Be explicit

--- src/main/scala/hbase/Value.scala
+++ src/main/scala/hbase/Value.scala
@@ -1,10 +1,10 @@
 package hbase
 
-import Bytes._
+import Bytes.StringBytes
 
 case class Value private[hbase] (_bytes: Array[Byte]) {
   def as[A](implicit valueC: Bytes[A]) = valueC.fromBytes(_bytes)
-  override def toString = as[String](implicitly[Bytes[String]])
+  override def toString = as[String](StringBytes)
 }
 
 object Value {
